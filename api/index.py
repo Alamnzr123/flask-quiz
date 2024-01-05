ed2c7b2c951dd439e6fde78a537024378ec5e912
Rahmad Alamsyah Nazaruddin <54619997+Alamnzr123@users.noreply.github.com>
2024-01-05 11:19:09 +0700

    changed file4

--- api/index.py
+++ api/index.py
@@ -1,6 +1,5 @@
-from api import routes
 from flask import Flask
-from config import Config
+from api.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -8,3 +7,4 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+from api import routes
