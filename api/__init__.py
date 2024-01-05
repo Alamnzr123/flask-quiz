b7375fbcfafaf292dc19a4659d25db00b6d870e4
Rahmad Alamsyah Nazaruddin <54619997+Alamnzr123@users.noreply.github.com>
2024-01-06 03:56:14 +0700

    add files12

--- api/__init__.py
+++ api/__init__.py
@@ -1,10 +1,25 @@
+# from flask import Flask
+# from config import Config
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_migrate import Migrate
+
+# app = Flask(__name__)
+# app.config.from_object(Config)
+# # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///home/alamnzr122/flask-quiz/app.db'
+# db = SQLAlchemy(app)
+# migrate = Migrate(app, db)
+# from api import index
+
+from api import index
 from flask import Flask
-from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config['SECRET_KEY'] = 'you-will-never-guess'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///home/alamnzr122/flask-quiz/app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['QUES_PER_PAGE'] = 1
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-from api import index
